5be107304273dd0a6b5e7e6d7e8ef2c721eec072
IsaevaAnastasiia <a.isaeva@mindbox.cloud>
2025-05-15 17:38:12 +0400

    fix: add move logic

--- src/agent/Agent.ts
+++ src/agent/Agent.ts
@@ -5,7 +5,7 @@ export enum AgentType {
 
 export class Agent {
   constructor(
-    public id: number,
+    public id: string,
     public type: AgentType,
     public radius: number,
     public position: [number, number],
@@ -13,13 +13,43 @@ export class Agent {
     private stamina: number
   ) {}
 
-  move(coordinates: Array<[number, number]>, onMove: () => void): void {}
+  public move(coordinates: Array<[number, number]>, onMove: () => void): void {
+    if (coordinates.length === 0) {
+      this.increaseStamina();
+      return;
+    }
 
-  increaseStamina(): void {}
+    if (coordinates.length > 1 && this.stamina > 0) {
+      coordinates.slice(0, 2).forEach((coordinate) => {
+        this.updatePosition(coordinate, onMove);
+      });
+      this.decreaseStamina();
+    } else {
+      this.updatePosition(coordinates[0], onMove);
+    }
+  }
 
-  decreaseStamina(): void {}
+  private updatePosition(
+    coordinate: [number, number],
+    onMove: () => void
+  ): void {
+    this.position = coordinate;
+    onMove();
+  }
 
-  mutate(): void {
+  private increaseStamina(): void {
+    if (this.stamina < this.maxStamina) {
+      this.stamina += 1;
+    }
+  }
+
+  private decreaseStamina(): void {
+    if (this.stamina > 0) {
+      this.stamina -= 1;
+    }
+  }
+
+  public mutate(): void {
     if (this.type === AgentType.ZOMBIE) return;
 
     this.type = AgentType.ZOMBIE;
