ff0ab8008443ceb1d7318cbac2b4204f31669d0d
Smolianinov Nikolay <n.smolianinov@mindbox.cloud>
2025-05-15 13:44:01 +0400

    init

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,99 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const config = {
+    tick: 1000,
+    size: 10
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const renderField = () => {
+    const {size} = config
+    if (size < 1 || size > 100) {
+        return alert('Ты лох');
+    }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+    const field = document.getElementById('field');
+
+    if (!field) {
+        throw new Error()
+    }
+
+    field.innerHTML = ''
+
+    field.style.gridTemplateRows = `repeat(${size}, minmax(15px, 1fr))`
+    field.style.gridTemplateColumns = `repeat(${size}, minmax(15px, 1fr))`
+
+    //    grid-template-columns: repeat(10, minmax(5px, 1fr));
+    //     grid-template-rows: repeat(10, minmax(5px, 1fr));
+
+    for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
+            const cell = document.createElement('div', {})
+
+            cell.id = `${x}:${y}`
+            cell.className = `cell`
+            field.appendChild(cell);
+        }
+    }
+}
+
+renderField()
+
+
+const button = document.getElementById('ok-button')
+
+
+button?.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const sizeInput = document.getElementById('size') as HTMLInputElement
+    const tickInput = document.getElementById('tick') as HTMLInputElement
+
+
+    const newSize = parseInt(sizeInput?.value)
+    config.tick = parseInt(tickInput?.value)
+
+    if (newSize !== config.size) {
+        config.size = newSize
+        renderField()
+    }
+
+    console.log(config)
+
+    paintCell()
+})
+
+
+let selectedCells = 0
+
+const paintCell = () => {
+
+    if (selectedCells >= config.size *config.size) {
+        return;
+    }
+
+    const randomX = Math.floor(Math.random() * config.size);
+    const randomY = Math.floor(Math.random() * config.size);
+
+    const id = `${randomX}:${randomY}`
+
+    console.log(id)
+
+    const targetCell = document.getElementById(id);
+
+
+    if (!targetCell) {
+        return
+    }
+
+    if (targetCell.classList.contains('selected')) {
+        return paintCell()
+    }
+
+    targetCell.className = 'selected';
+    selectedCells += 1
+
+
+    setTimeout(() => {
+        paintCell()
+    }, config.tick)
+}
+
