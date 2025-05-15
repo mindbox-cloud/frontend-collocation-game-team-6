82b13c1102550cac16fa7620de8207d05de2af85
IsaevaAnastasiia <a.isaeva@mindbox.cloud>
2025-05-15 17:07:41 +0400

    add agent class

--- src/agent/Agent.ts
+++ src/agent/Agent.ts
@@ -1,4 +1,21 @@
 export enum AgentType {
-    HUMAN = 'HUMAN',
-	ZOMBIE = 'ZOMBIE'
+  HUMAN = "HUMAN",
+  ZOMBIE = "ZOMBIE",
+}
+
+export class Agent {
+  constructor(
+    public id: number,
+    public type: AgentType,
+    public radius: number,
+    public maxStamina: number,
+    public stamina: number,
+    public position: [number, number]
+  ) {}
+
+  move(coordinates: Array<[number, number]>, onMove: () => void): void {}
+
+  updateStamina(): void {}
+
+  mutate(): void {}
 }
